f74a21e579f6fc72694a4696c2ec5c5d50fef9fd
Татьяна Ганя <121543437+TatyanaGanya@users.noreply.github.com>
2023-03-15 00:36:19 +0300

    исправлен код js

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,8 @@ const popupOpenButtomElement = document.querySelector(".profile__edit");
 
 const openPopup = function () {
     popupElement.classList.add("popup_open");
+    nameInput.value=profileName.innerText;
+    jobInput.value=profileAbout.innerText;
 };
 
 const closePopup = function () {
@@ -14,37 +16,26 @@ const closePopup = function () {
 };
 
 // open and close
-
 popupOpenButtomElement.addEventListener('click', openPopup);
 popupClouseButtomElement.addEventListener('click', closePopup);
 
 
 // Находим форму в DOM
 let formElement = popupElement.querySelector('.popup__content');
-
 let nameInput = popupElement.querySelector('.popup__input_type_name');
 let jobInput = popupElement.querySelector('.popup__input_type_job');
-
 let profileName = document.querySelector('.profile__name')
 let profileAbout = document.querySelector('.profile__about')
 
 
-
-//innerText
-nameInput.value=profileName.innerText;
-jobInput.value=profileAbout.innerText;
-
 function handleFormSubmit (evt) {
-
-
+ 
     evt.preventDefault();
 
-    nameInput.value = profileName.textContent = nameInput.value;
+    profileName.textContent = nameInput.value;
     profileAbout.textContent = jobInput.value;
     closePopup ();
-    
 };
 
 // Прикрепляем обработчик к форме:
-
 formElement.addEventListener('submit', handleFormSubmit); 
