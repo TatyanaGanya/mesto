c422a10c31e2d239b66470effe9ba24ebf6b1c6e
Татьяна Ганя <121543437+TatyanaGanya@users.noreply.github.com>
2023-05-17 12:52:54 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,18 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(?:js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      },
+      {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, {
           loader: 'css-loader',
